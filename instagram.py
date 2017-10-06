86c9faecad9a55cadd5401996e71b8a3c226e045
karubian <burake1009@gmail.com>
2017-10-06 21:00:08 +0300

    instagram updated,config file added

--- instagram.py
+++ instagram.py
@@ -1,10 +1,49 @@
-from instagram.client import InstagramAPI
+import requests
 
-access_token = "6168565408.1677ed0.fb723d37d2a5470cb54dc279942a42cb"
-client_secret = "7d86390e8a3d42c7a0197ab1e8fcd079"
-api = InstagramAPI(access_token=access_token, client_secret=client_secret)
-recent_media, next_ = api.user_recent_media(user_id="berksefkatli", count=10)
-for media in recent_media:
-    print(media.caption.text)
+APP_ACCESS_TOKEN = '6168565408.4a4eeb7.ccac4ddd5dbd474085db8c6d99c4d32e'
+BASE_URL = 'https://api.instagram.com/v1/'
 
 
+def self_info():
+  request_url = (BASE_URL + 'users/self/?access_token=%s') % APP_ACCESS_TOKEN
+  print('GET request url : %s' % request_url)
+  user_info = requests.get(request_url).json()
+
+  if user_info['meta']['code'] == 200:
+    if len(user_info['data']):
+      print('Username: %s' % (user_info['data']['username']))
+      print('No. of followers: %s' % (user_info['data']['counts']['followed_by']))
+      print('No. of people you are following: %s' % (user_info['data']['counts']['follows']))
+      print('No. of posts: %s' % (user_info['data']['counts']['media']))
+    else:
+      print('User does not exist!')
+  else:
+    print ('Status code other than 200 received!')
+
+
+def get_user_id(insta_username):
+  request_url = (BASE_URL + 'users/search?q=%s&access_token=%s') % (insta_username, APP_ACCESS_TOKEN)
+  print('GET request url : %s' % (request_url))
+  user_info = requests.get(request_url).json()
+
+  if user_info['meta']['code'] == 200:
+    if len(user_info['data']):
+      return user_info['data'][0]['id']
+    else:
+      return None
+  else:
+    print('Status code other than 200 received!')
+    exit()
+
+def get_user_info(insta_username):
+  user_id = get_user_id(insta_username)
+  if user_id == None:
+    print('User does not exist!')
+    exit()
+  request_url = (BASE_URL + 'users/%s?access_token=%s') % (user_id, APP_ACCESS_TOKEN)
+  print('GET request url : %s' % (request_url))
+  user_info = requests.get(request_url).json()
+  print(user_info)
+get_user_info("berksefkatli")
+
+self_info()
--- twitter.py
+++ twitter.py
@@ -1,21 +1,19 @@
 import tweepy
+import twitter_config as tw
 
-consumer_key = "iJp4X3rXDw59Wk48dS9o8Krgr"
-consumer_secret = "NumnqCgZvwqpF402JbcgCWPJPR44uThju83yhyF3ewjXES3LoC"
-access_key = "916015993016717313-1C329HlKA5tbAjFYvTnVXzG6rk8PNLz"
-access_secret = "JmYVfB8zABEminF0JKXtK5GDZTEtjGA1gO3rQ3jHrCxkQ"
+
 
 try:
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_key, access_secret)
+    auth = tweepy.OAuthHandler(tw.consumer_key, tw.consumer_secret)
+    auth.set_access_token(tw.access_key, tw.access_secret)
     auth.get_authorization_url()
     api = tweepy.API(auth)
 except tweepy.TweepError:
     print('Hata')
 
-user = api.get_user("kesinlikleburak")  # kullanıcıyı ogunal00 olarak seçtik
+user = api.get_user("projectpaparazz")
 i=0
-public_tweets = api.user_timeline(screen_name = "projectpaparazz",count=10) # ogunal00 kullanıcısının 5 adet tweeti
+public_tweets = api.user_timeline(screen_name = "projectpaparazz",count=10)
 for tweet in public_tweets:
         print(tweet.created_at)
         print(i)
