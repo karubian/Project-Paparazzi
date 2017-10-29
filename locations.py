b5b779740e506409148d3c4c8f762dae70bcb17a
ebruucgun <ucgunebru@gmail.com>
2017-10-29 15:47:49 +0300

    Türkiye il ve ilçeleri

---
+++ locations.py
@@ -0,0 +1,14 @@
+import csv
+import pandas as pd
+import io
+
+with open("TR.csv", 'rb') as csvfile:
+    df = pd.read_csv(csvfile,encoding="WINDOWS-1254",delimiter=";")
+
+tr = [tuple(x) for x in df.values]
+
+# for i in range(len(tuples)):
+#   a, b = tuples[i]
+#   print(a,b)
+
+print(tr)
