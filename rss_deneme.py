b71ccf582bd2a2dd4fc74cba249baf9ba7e0f03b
Berk Şefkatli <berksefkatli@gmail.com>
2017-11-18 23:34:35 +0300

    rss threading sabah haberturk finds new articles

--- rss_deneme.py
+++ rss_deneme.py
@@ -1,30 +1,48 @@
 import feedparser
+import threading
 import time
-from threading import Thread
 
 thread_pool = [];
-sabah = "https://www.sabah.com.tr/rss/gunaydin.xml"
-#hurriyet = "https://www.hurriyet.com.tr/rss/magazin"
-#hürriyet eski haberleri veriyor rssi düzgün değil
+sabah = "https://www.sabah.com.tr/rss/sondakika.xml"
+# hurriyet = "https://www.hurriyet.com.tr/rss/magazin"
+# hürriyet eski haberleri veriyor rssi düzgün değil
 haberturk = "http://www.haberturk.com/rss/magazin.xml"
 
-def get_rss(rss_url, rss_item_count):
+
+
+
+def get_rss(rss_url):
+    rss_array = [];
     d = feedparser.parse(rss_url)
-    print(d['feed']['title'])
-    print(len(d['entries']))
-    for i in range(rss_item_count):
-        print(d['entries'][i]['title'])
-        print(d['entries'][i]['link'])
+    rss_array = d['entries'].copy()
+    while (1):
+        d = feedparser.parse(rss_url)
+        rss_item_count = len(d['entries'])
+        if (rss_array[0]['link'] == d['entries'][0]['link']):
+            # It means no new rss item
+            print("no new")
+        else:
+            # print(d['feed']['title'])
+            # print(len(d['entries']))
+            for i in range(rss_item_count):
+                if (rss_array[i]['link'] != d['entries'][i]['link']):
+                    continue
+                print(i + " tane yeni haber")
+                for j in range(i):
+                    print("write to database " + j)
+                    print(d['entries'][j]['link'])
+                # print(d['entries'][i]['title'])
+                # print(d['entries'][i]['link'])
+        print('sleep for 5 minutes')
+        time.sleep(300)
+        print('i woke up')
 
 
 try:
-    thread_pool.append(Thread(target=get_rss, args=(sabah,30)))
-    #thread_pool.append(Thread(target=get_rss, args=(hurriyet,49)))
-    thread_pool.append(Thread(target=get_rss, args=(haberturk,20)))
+    thread_pool.append(threading.Thread(target=get_rss, args=(sabah,)))
+    # thread_pool.append(threading.Thread(target=get_rss, args=(hurriyet)))
+    thread_pool.append(threading.Thread(target=get_rss, args=(haberturk,)))
 
-    # _thread.start_new_thread(get_rss, (sabah, 30))
-    # _thread.start_new_thread(get_rss, (hurriyet, 49,))
-    # _thread.start_new_thread(get_rss, (haberturk, 20,))
 except:
     print("za")
 
@@ -33,25 +51,3 @@ for elem in thread_pool:
 
 for elem in thread_pool:
     elem.join()
-
-
-# import _thread
-# import time
-#
-# # Define a function for the thread
-# def print_time( threadName, delay):
-#    count = 0
-#    while count < 5:
-#       time.sleep(delay)
-#       count += 1
-#       print ("%s: %s" % ( threadName, time.ctime(time.time()) ))
-#
-# # Create two threads as follows
-# try:
-#    _thread.start_new_thread( print_time, ("Thread-1", 2, ) )
-#    _thread.start_new_thread( print_time, ("Thread-2", 4, ) )
-# except:
-#    print ("Error: unable to start thread")
-#
-# while 1:
-#    pass
