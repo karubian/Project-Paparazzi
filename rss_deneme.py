605ccc1735a84f64d78140623725199e1127cd48
Berk Şefkatli <berksefkatli@gmail.com>
2017-11-19 00:58:08 +0300

    rss threading sabah haberturk finds new articles with better prints and exception handling

--- rss_deneme.py
+++ rss_deneme.py
@@ -9,38 +9,39 @@ sabah = "https://www.sabah.com.tr/rss/sondakika.xml"
 haberturk = "http://www.haberturk.com/rss/magazin.xml"
 
 
-
-
 def get_rss(rss_url):
     rss_array = [];
-    d = feedparser.parse(rss_url)
-    rss_array = d['entries'].copy()
-    while (1):
+    try:
         d = feedparser.parse(rss_url)
-        rss_item_count = len(d['entries'])
-        if (rss_array[0]['link'] == d['entries'][0]['link']):
-            # It means no new rss item
-            print("no new")
-        else:
-            # print(d['feed']['title'])
-            # print(len(d['entries']))
-            for i in range(rss_item_count):
-                if (rss_array[i]['link'] != d['entries'][i]['link']):
-                    continue
-                print(i + " tane yeni haber")
+        rss_array = d['entries'].copy()
+        print(threading.currentThread().name + " is working on RSS URL: " + rss_url)
+        while (1):
+            d = feedparser.parse(rss_url)
+            rss_item_count = len(d['entries'])
+            if (rss_array[0]['link'] == d['entries'][0]['link']):
+                # It means no new rss item
+                print("No new articles in " + threading.currentThread().name)
+            else:
+                for i in range(1,rss_item_count):
+                    if (rss_array[i]['link'] == d['entries'][i]['link']):
+                        break
+                print("Found " + str(i) + " new articles in " + threading.currentThread().name)
                 for j in range(i):
-                    print("write to database " + j)
                     print(d['entries'][j]['link'])
-                # print(d['entries'][i]['title'])
-                # print(d['entries'][i]['link'])
-        print('sleep for 5 minutes')
+            print(threading.currentThread().name + ' is sleeping for 5 minutes')
+            time.sleep(300)
+            print(threading.currentThread().name + ' has woken up')
+    except:
+        print("Something went wrong in " + threading.currentThread().name)
+        print(threading.currentThread().name + ' is sleeping for 5 minutes')
         time.sleep(300)
-        print('i woke up')
+        print(threading.currentThread().name + ' has woken up')
+        get_rss(rss_url)
 
 
 try:
     thread_pool.append(threading.Thread(target=get_rss, args=(sabah,)))
-    # thread_pool.append(threading.Thread(target=get_rss, args=(hurriyet)))
+    #thread_pool.append(threading.Thread(target=get_rss, args=(hurriyet)))
     thread_pool.append(threading.Thread(target=get_rss, args=(haberturk,)))
 
 except:
