8378b9e7139f4228bce4b443cec4f06910d54555
Berk Şefkatli <berksefkatli@gmail.com>
2017-11-19 19:08:23 +0300

    rss sorted

--- rss_deneme.py
+++ rss_deneme.py
@@ -2,34 +2,38 @@ import feedparser
 import threading
 import time
 
-thread_pool = [];
-sabah = "https://www.sabah.com.tr/rss/sondakika.xml"
+thread_pool = []
+sabah = "https://www.sabah.com.tr/rss/anasayfa.xml"
+sabah2 = "https://www.sabah.com.tr/rss/sondakika.xml"
+haberturk = "http://www.haberturk.com/rss/magazin.xml"
+anadoluajans = "http://aa.com.tr/tr/rss/default?cat=guncel"
 # hurriyet = "https://www.hurriyet.com.tr/rss/magazin"
 # hürriyet eski haberleri veriyor rssi düzgün değil
-haberturk = "http://www.haberturk.com/rss/magazin.xml"
 
 
 def get_rss(rss_url):
-    rss_array = [];
     try:
-        d = feedparser.parse(rss_url)
-        rss_array = d['entries'].copy()
+        new_rss = feedparser.parse(rss_url)
+        sorted_old_rss_array = sorted(new_rss['entries'], key=lambda article: article['published_parsed'],
+                                      reverse=True)
         print(threading.currentThread().name + " is working on RSS URL: " + rss_url)
-        while (1):
-            d = feedparser.parse(rss_url)
-            rss_item_count = len(d['entries'])
-            if (rss_array[0]['link'] == d['entries'][0]['link']):
+        while True:
+            new_rss = feedparser.parse(rss_url)
+            rss_item_count = len(new_rss['entries'])
+            sorted_new_rss_array = sorted(new_rss['entries'], key=lambda article: article['published_parsed'],
+                                          reverse=True)
+            if sorted_old_rss_array[0]['link'] == sorted_new_rss_array[0]['link']:
                 # It means no new rss item
                 print("No new articles in " + threading.currentThread().name)
             else:
                 for i in range(1,rss_item_count):
-                    if (rss_array[i]['link'] == d['entries'][i]['link']):
+                    if sorted_old_rss_array[0]['link'] == sorted_new_rss_array[i]['link']:
                         break
                 print("Found " + str(i) + " new articles in " + threading.currentThread().name)
                 for j in range(i):
                     # write the new articles to the database here
-                    print(d['entries'][j]['link'])
-                rss_array = d['entries'].copy()
+                    print(sorted_new_rss_array[j]['link'])
+                sorted_old_rss_array = sorted_new_rss_array.copy()
             print(threading.currentThread().name + ' is sleeping for 5 minutes')
             time.sleep(300)
             print(threading.currentThread().name + ' has woken up')
@@ -43,11 +47,12 @@ def get_rss(rss_url):
 
 try:
     thread_pool.append(threading.Thread(target=get_rss, args=(sabah,)))
-    #thread_pool.append(threading.Thread(target=get_rss, args=(hurriyet)))
+    thread_pool.append(threading.Thread(target=get_rss, args=(sabah2,)))
     thread_pool.append(threading.Thread(target=get_rss, args=(haberturk,)))
+    thread_pool.append(threading.Thread(target=get_rss, args=(anadoluajans,)))
 
 except:
-    print("za")
+    print("Creating threads failed.")
 
 for elem in thread_pool:
     elem.start()
