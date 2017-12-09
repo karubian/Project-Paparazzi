2030eea884ce3323c76de6902bc94667ddc35f17
burakedu <b21427162@cs.hacettepe.edu.tr>
2017-12-09 10:32:42 +0300

    Directory structure established.

--- twitter_config.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-consumer_key = "iJp4X3rXDw59Wk48dS9o8Krgr"
-consumer_secret = "NumnqCgZvwqpF402JbcgCWPJPR44uThju83yhyF3ewjXES3LoC"
-access_key = "916015993016717313-1C329HlKA5tbAjFYvTnVXzG6rk8PNLz"
-access_secret = "JmYVfB8zABEminF0JKXtK5GDZTEtjGA1gO3rQ3jHrCxkQ"
